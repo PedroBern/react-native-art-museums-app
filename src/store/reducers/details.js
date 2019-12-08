b3d8f8d0a15f9dad2e4fa0651303227bec7c3748
Pedro Bern <pedrobermoreira@gmail.com>
2019-12-08 18:29:10 -0200

    remove console.log

--- src/store/reducers/details.js
+++ src/store/reducers/details.js
@@ -7,7 +7,6 @@ export const detailsInitialState = {
 const detailsReducer = (state = detailsInitialState, action) => {
   switch (action.type) {
     case SET_CURRENT_DETAIL:
-      console.log("id is", state.id);
       return { ...state, id: action.payload };
 
     case RESET_CURRENT_DETAIL:
