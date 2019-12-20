a44a2544e7f598c75e632374cd7d76bb76e5405e
Pedro Bern <pedrobermoreira@gmail.com>
2019-12-20 17:38:07 -0300

    run ios tests

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ const { withEnzyme } = require("jest-expo-enzyme");
 
 module.exports = {
   projects: [
-    // withEnzyme(require('jest-expo/ios/jest-preset')),
+    withEnzyme(require("jest-expo/ios/jest-preset")),
     withEnzyme(require("jest-expo/android/jest-preset"))
   ],
   collectCoverageFrom: [
